c9c68c005fb8cbc5943ace633e7001611e91d67b
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-08-31 18:37:12 -0300

    advances

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import router from './routes/index.js';
+import logger from 'morgan'
 
 const app = express()
 
+app.use(express.json())
 app.use(express.json());
 app.use(cors())
 app.use(helmet({
   crossOriginResourcePolicy: false,
 }));
+process.env.NODE_ENV === 'development' && app.use(logger('dev'))
 
 app.use('/api/v1', router)
 
 app.get('/', (req, res) => {
-  return res.send(`<h1>${process.env.SALUD}</h1>`)
+  return res.send(`<h1>Hello Word</h1>`)
 })
 
 export default app
