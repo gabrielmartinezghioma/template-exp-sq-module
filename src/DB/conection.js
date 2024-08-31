aa822bee3ca09fa2f280611b8119293442766c66
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-08-31 14:10:41 -0300

    advances

--- src/DB/conection.js
+++ src/DB/conection.js
@@ -1,18 +1,21 @@
-// import { Sequelize } from 'sequelize';
-
-// const DB = process.env.POSTGRES_DATABASE
-// const sequelize = new Sequelize(DB, { logging: false })
-
-// export default sequelize
-
+import { Sequelize } from 'sequelize';
 import pg from 'pg';
+import PgHstore from 'pg-hstore';
 
-const { Pool } = pg;
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_DATABASE_URL,
-})
+const DB = process.env.POSTGRES_DATABASE
+const sequelize = new Sequelize(DB, { logging: false })
 
-console.log(pool);
+export default sequelize
 
-export default pool
+// import pg from 'pg';
+
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_DATABASE_URL,
+// })
+
+// console.log(pool);
+
+// export default pool
