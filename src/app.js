b546cc7ea3ff66d39bbcdb4c1366bb69f5229fbf
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-08-31 18:25:10 -0300

    advances

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(helmet({
 app.use('/api/v1', router)
 
 app.get('/', (req, res) => {
-  return res.send(`<h1>aa</h1>`)
+  return res.send(`<h1>${process.env.SALUD}</h1>`)
 })
 
 export default app
